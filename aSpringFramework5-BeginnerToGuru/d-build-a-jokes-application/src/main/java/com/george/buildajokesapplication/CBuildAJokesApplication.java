fca06471b5589a76f38dacce43a6e80f5e4693b1
George <gheorghita.harpa0423@yahoo.com>
2020-06-29 23:45:51 +0100

    Building a Spring Boot Jokes Application: module setup.

---
+++ aSpringFramework5-BeginnerToGuru/d-build-a-jokes-application/src/main/java/com/george/buildajokesapplication/CBuildAJokesApplication.java
@@ -0,0 +1,13 @@
+package com.george.buildajokesapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CBuildAJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CBuildAJokesApplication.class, args);
+    }
+
+}
---
+++ aSpringFramework5-BeginnerToGuru/d-build-a-jokes-application/src/test/java/com/george/buildajokesapplication/CBuildAJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.buildajokesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CBuildAJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
