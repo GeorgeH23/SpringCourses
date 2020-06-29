5412e0ec9c7e36db90eb394ccec8929a8371cc03
George <gheorghita.harpa0423@yahoo.com>
2020-06-29 23:15:24 +0100

    Spring Pet Clinic - Implement Base Entity. Closed #9.

--- aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/model/Pet.java
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/model/Pet.java
@@ -2,7 +2,7 @@ package com.george.petclinicapplication.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/model/PetType.java
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/model/PetType.java
@@ -1,6 +1,6 @@
 package com.george.petclinicapplication.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
