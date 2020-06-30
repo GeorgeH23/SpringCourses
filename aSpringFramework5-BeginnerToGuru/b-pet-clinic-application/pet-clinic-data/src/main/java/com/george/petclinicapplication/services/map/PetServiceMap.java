fb466d0715d4c1276387f7dabf82ea5d4c676c89
George <gheorghita.harpa0423@yahoo.com>
2020-06-30 20:32:11 +0100

    Spring Pet Clinic - Implement Map Based Services. Closes #5.

---
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.george.petclinicapplication.services.map;
+
+import com.george.petclinicapplication.model.Owner;
+import com.george.petclinicapplication.model.Pet;
+import com.george.petclinicapplication.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package com.george.petclinicapplication.services.map;
+
+import com.george.petclinicapplication.model.Owner;
+import com.george.petclinicapplication.model.Vet;
+import com.george.petclinicapplication.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+
+}
