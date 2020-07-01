c49c3187e88fa688a60e6e76ecf151db8e39192d
George <gheorghita.harpa0423@yahoo.com>
2020-07-01 21:57:19 +0100

    Java Configuration Example.

--- aSpringFramework5-BeginnerToGuru/d-build-a-jokes-application/src/main/java/com/george/buildajokesapplication/BuildAJokesApplication.java
+++ aSpringFramework5-BeginnerToGuru/d-build-a-jokes-application/src/main/java/com/george/buildajokesapplication/BuildAJokesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CBuildAJokesApplication {
+public class BuildAJokesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CBuildAJokesApplication.class, args);
+        SpringApplication.run(BuildAJokesApplication.class, args);
 
         // Custom Banner Link: http://patorjk.com/software/taag/#p=display&f=Big%20Money-ne&t=%20%20Spring%20%0A%20%20%20%20%20Jokes%0AApplication
     }
