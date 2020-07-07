7db53bb3d0d07073b087a7041eafb7d05134da5a
George <gheorghita.harpa0423@yahoo.com>
2020-07-07 21:17:44 +0100

    Multiple Properties Files.

--- aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/FactoryBeanApplication.java
+++ aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/FactoryBeanApplication.java
@@ -2,6 +2,7 @@ package com.george.factorybeans;
 
 import com.george.factorybeans.controllers.MyController;
 import com.george.factorybeans.examplebeans.FakeDataSource;
+import com.george.factorybeans.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,8 +16,10 @@ public class FactoryBeanApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
-
 		System.out.println(fakeDataSource.getUsername());
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
 
 	}
 }
--- aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/config/PropertyConfig.java
+++ aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package com.george.factorybeans.config;
 
 import com.george.factorybeans.examplebeans.FakeDataSource;
+import com.george.factorybeans.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -25,6 +31,15 @@ public class PropertyConfig {
     @Value("${george.dburl}")
     String url;
 
+    @Value("${george.jms.username}")
+    String jmsUsername;
+
+    @Value("${george.jms.password}")
+    String jmsPassword;
+
+    @Value("${george.jms.dburl}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -34,6 +49,16 @@ public class PropertyConfig {
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassword);
+        jmsBroker.setUrl(jmsUrl);
+
+        return jmsBroker;
     }
 
     // the PropertySourcesPlaceholderConfigurer will read the properties file
