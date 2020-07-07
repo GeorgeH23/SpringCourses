a1045ea554c17a4d1f407fdaf983928c77efb059
George <gheorghita.harpa0423@yahoo.com>
2020-07-07 20:50:27 +0100

    Spring Environment Properties.

--- aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/config/PropertyConfig.java
+++ aSpringFramework5-BeginnerToGuru/f-external-properties-with-spring-framework/src/main/java/com/george/factorybeans/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.george.factorybeans.config;
 
 import com.george.factorybeans.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${george.username}")
     String username;
@@ -23,7 +28,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        //fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
