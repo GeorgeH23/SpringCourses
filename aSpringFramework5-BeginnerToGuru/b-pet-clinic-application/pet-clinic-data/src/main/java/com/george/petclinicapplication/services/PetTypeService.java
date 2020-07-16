13753e708e382fe611bb729c4bf4f16468293125
George Harpa <gheorghita.harpa0423@yahoo.com>
2020-07-16 10:25:33 +0100

    Spring Pet Clinic - Create Pet Type Map Service. Closes #29.

---
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.george.petclinicapplication.services;
+
+import com.george.petclinicapplication.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
