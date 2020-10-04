0c7948ffd2ebe68f59534ff3475ebebd74220568
George <gheorghita.harpa0423@yahoo.com>
2020-10-04 22:12:08 +0100

    Display List of Recipes.

--- aSpringFramework5-BeginnerToGuru/h-jpa-data-modeling-with-spring-and-hibernate/src/main/java/com/george/recipeapp/controllers/IndexController.java
+++ aSpringFramework5-BeginnerToGuru/h-jpa-data-modeling-with-spring-and-hibernate/src/main/java/com/george/recipeapp/controllers/IndexController.java
@@ -4,7 +4,9 @@ import com.george.recipeapp.domain.Category;
 import com.george.recipeapp.domain.UnitOfMeasure;
 import com.george.recipeapp.repositories.CategoryRepository;
 import com.george.recipeapp.repositories.UnitOfMeasureRepository;
+import com.george.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,25 +14,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        /*System.out.println("Category ID is: " + categoryOptional.get().getId());
-        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());*/
-
-        categoryOptional.ifPresent(category -> System.out.println("Category ID is: " + category.getId()));
-        unitOfMeasureOptional.ifPresent(unitOfMeasure -> System.out.println("UOM ID is: " + unitOfMeasure.getId()));
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
---
+++ aSpringFramework5-BeginnerToGuru/h-jpa-data-modeling-with-spring-and-hibernate/src/main/java/com/george/recipeapp/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.george.recipeapp.services;
+
+import com.george.recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
