c71994706d309cf2de24e39b98b01dce81b980e4
George <gheorghita.harpa0423@yahoo.com>
2020-10-09 23:20:23 +0100

    Complete Refactoring with Project Lombok.

--- aSpringFramework5-BeginnerToGuru/i-project-lombok/src/main/java/com/george/recipeapp/controllers/IndexController.java
+++ aSpringFramework5-BeginnerToGuru/i-project-lombok/src/main/java/com/george/recipeapp/controllers/IndexController.java
@@ -1,16 +1,13 @@
 package com.george.recipeapp.controllers;
 
-import com.george.recipeapp.domain.Category;
-import com.george.recipeapp.domain.UnitOfMeasure;
-import com.george.recipeapp.repositories.CategoryRepository;
-import com.george.recipeapp.repositories.UnitOfMeasureRepository;
 import com.george.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting Index page.");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
