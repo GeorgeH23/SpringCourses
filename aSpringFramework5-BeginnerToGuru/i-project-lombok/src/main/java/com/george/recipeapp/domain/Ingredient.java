e77048a9245793b0427fce065b3e11595bfa1655
George Harpa <gheorghita.harpa0423@yahoo.com>
2020-10-10 11:48:29 +0100

    Spring Pet Clinic - Refactoring for Project Lombok. Closes #51

--- aSpringFramework5-BeginnerToGuru/i-project-lombok/src/main/java/com/george/recipeapp/domain/Ingredient.java
+++ aSpringFramework5-BeginnerToGuru/i-project-lombok/src/main/java/com/george/recipeapp/domain/Ingredient.java
@@ -18,6 +18,7 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private BigDecimal amount;
 
