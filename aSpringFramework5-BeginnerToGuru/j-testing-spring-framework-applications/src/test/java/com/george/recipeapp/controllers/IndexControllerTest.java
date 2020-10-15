42272839a6c98b0d9f4d90d3e09a1d09a76834cc
George <gheorghita.harpa0423@yahoo.com>
2020-10-15 22:10:53 +0100

    Introduction to Spring MockMVC.

--- aSpringFramework5-BeginnerToGuru/j-testing-spring-framework-applications/src/test/java/com/george/recipeapp/controllers/IndexControllerTest.java
+++ aSpringFramework5-BeginnerToGuru/j-testing-spring-framework-applications/src/test/java/com/george/recipeapp/controllers/IndexControllerTest.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-class IndexControllerTest {
+public class IndexControllerTest {
 
     @Mock
     RecipeService recipeService;
@@ -28,14 +32,23 @@ class IndexControllerTest {
     IndexController controller;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
 
         controller = new IndexController(recipeService);
     }
 
     @Test
-    void getIndexPage() {
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexPage() {
 
         //given
         Set<Recipe> recipes = new HashSet<>();
