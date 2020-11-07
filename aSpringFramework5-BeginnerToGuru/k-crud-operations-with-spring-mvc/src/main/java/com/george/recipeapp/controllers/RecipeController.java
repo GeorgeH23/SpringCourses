4334e1dd514eeba85e6555cacbf9d8a3faac8273
George <gheorghita.harpa0423@yahoo.com>
2020-11-07 23:25:10 +0000

    Delete a Recipe + Update recipeform.html to update recipe difficulty.

--- aSpringFramework5-BeginnerToGuru/k-crud-operations-with-spring-mvc/src/main/java/com/george/recipeapp/controllers/RecipeController.java
+++ aSpringFramework5-BeginnerToGuru/k-crud-operations-with-spring-mvc/src/main/java/com/george/recipeapp/controllers/RecipeController.java
@@ -1,16 +1,18 @@
 package com.george.recipeapp.controllers;
 
 import com.george.recipeapp.commands.RecipeCommand;
+import com.george.recipeapp.domain.Difficulty;
 import com.george.recipeapp.services.CategoryService;
 import com.george.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @Controller
+@Slf4j
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -25,6 +27,7 @@ public class RecipeController {
         this.categoryService = categoryService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute(RECIPE_ATTRIBUTE, recipeService.findById(Long.parseLong(id)));
@@ -32,6 +35,7 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute(RECIPE_ATTRIBUTE, new RecipeCommand());
@@ -40,6 +44,7 @@ public class RecipeController {
         return RECIPE_RECIPE_FORM_URL;
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute(RECIPE_ATTRIBUTE, recipeService.findCommandById(Long.valueOf(id)));
@@ -49,10 +54,20 @@ public class RecipeController {
     }
 
     //@RequestMapping(name = "recipe", method = RequestMethod.POST) -OLD Spring 4 Way
-    @PostMapping(RECIPE_ATTRIBUTE)
+    @PostMapping
+    @RequestMapping(RECIPE_ATTRIBUTE)
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping(value = "recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
