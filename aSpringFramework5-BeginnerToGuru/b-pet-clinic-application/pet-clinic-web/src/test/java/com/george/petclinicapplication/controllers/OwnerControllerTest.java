10356a52ea89e70a9f78872afaf5f0b3e1437ad3
George <gheorghita.harpa0423@yahoo.com>
2020-11-29 19:41:08 +0000

    Spring Pet Clinic - Display Owner Record. Closes #53

--- aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-web/src/test/java/com/george/petclinicapplication/controllers/OwnerControllerTest.java
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-web/src/test/java/com/george/petclinicapplication/controllers/OwnerControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -69,5 +69,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(3L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(3L))));
     }
 }
