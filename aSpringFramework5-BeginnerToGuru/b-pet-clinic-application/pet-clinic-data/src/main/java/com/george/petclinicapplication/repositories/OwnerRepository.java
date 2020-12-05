d3fb1dfe8adc7f5e724c7474b0f3f3b759b4b5ae
George <gheorghita.harpa0423@yahoo.com>
2020-12-05 20:59:11 +0000

    Spring Pet Clinic - Implement Find Owner. Closes #54

--- aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/repositories/OwnerRepository.java
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.george.petclinicapplication.repositories;
 import com.george.petclinicapplication.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/services/OwnerService.java
+++ aSpringFramework5-BeginnerToGuru/b-pet-clinic-application/pet-clinic-data/src/main/java/com/george/petclinicapplication/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.george.petclinicapplication.services;
 
 import com.george.petclinicapplication.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
