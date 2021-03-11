fbe7233443e4501e1a58a2c874c232870e2a0f6c
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-03-11 22:30:53 +0000

    Refactor Database Initialization for MySQL.

--- aSpringFramework5-BeginnerToGuru/m-using-mysql-with-spring-boot/src/main/java/com/george/recipeapp/bootstrap/BootStrapMySQL.java
+++ aSpringFramework5-BeginnerToGuru/m-using-mysql-with-spring-boot/src/main/java/com/george/recipeapp/bootstrap/BootStrapMySQL.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,28 +18,93 @@ import java.util.Optional;
 
 @Slf4j
 @Component
-@Profile("default")
-public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+@Profile({"dev", "prod"})
+public class BootStrapMySQL implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final String EXPECTED_UOM_NOT_FOUND = "Expected UOM Not Found";
     private final CategoryRepository categoryRepository;
-    private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeRepository recipeRepository;
 
-    public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public BootStrapMySQL(CategoryRepository categoryRepository,
+                          UnitOfMeasureRepository unitOfMeasureRepository, RecipeRepository recipeRepository) {
         this.categoryRepository = categoryRepository;
-        this.recipeRepository = recipeRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     @Override
-    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        recipeRepository.saveAll(getRecipes());
-        log.debug("Loading Bootstrap data.");
+
+        if (categoryRepository.count() == 0L){
+            log.debug("Loading Categories");
+            loadCategories();
+        }
+
+        if (unitOfMeasureRepository.count() == 0L){
+            log.debug("Loading UOMs");
+            loadUom();
+        }
+
+        if (recipeRepository.count() == 0L) {
+            log.debug("Loading Recipes");
+            recipeRepository.saveAll(loadRecipes());
+        }
     }
 
-    private List<Recipe> getRecipes() {
+    private void loadCategories(){
+        Category cat1 = new Category();
+        cat1.setDescription("American");
+        categoryRepository.save(cat1);
+
+        Category cat2 = new Category();
+        cat2.setDescription("Italian");
+        categoryRepository.save(cat2);
+
+        Category cat3 = new Category();
+        cat3.setDescription("Mexican");
+        categoryRepository.save(cat3);
+
+        Category cat4 = new Category();
+        cat4.setDescription("Fast Food");
+        categoryRepository.save(cat4);
+    }
+
+    private void loadUom(){
+        UnitOfMeasure uom1 = new UnitOfMeasure();
+        uom1.setDescription("Teaspoon");
+        unitOfMeasureRepository.save(uom1);
+
+        UnitOfMeasure uom2 = new UnitOfMeasure();
+        uom2.setDescription("Tablespoon");
+        unitOfMeasureRepository.save(uom2);
+
+        UnitOfMeasure uom3 = new UnitOfMeasure();
+        uom3.setDescription("Cup");
+        unitOfMeasureRepository.save(uom3);
+
+        UnitOfMeasure uom4 = new UnitOfMeasure();
+        uom4.setDescription("Pinch");
+        unitOfMeasureRepository.save(uom4);
+
+        UnitOfMeasure uom5 = new UnitOfMeasure();
+        uom5.setDescription("Ounce");
+        unitOfMeasureRepository.save(uom5);
+
+        UnitOfMeasure uom6 = new UnitOfMeasure();
+        uom6.setDescription("Each");
+        unitOfMeasureRepository.save(uom6);
+
+        UnitOfMeasure uom7 = new UnitOfMeasure();
+        uom7.setDescription("Pint");
+        unitOfMeasureRepository.save(uom7);
+
+        UnitOfMeasure uom8 = new UnitOfMeasure();
+        uom8.setDescription("Dash");
+        unitOfMeasureRepository.save(uom8);
+    }
+
+    private List<Recipe> loadRecipes() {
 
         List<Recipe> recipes = new ArrayList<>(2);
 
@@ -214,5 +278,4 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         recipes.add(tacosRecipe);
         return recipes;
     }
-
 }
