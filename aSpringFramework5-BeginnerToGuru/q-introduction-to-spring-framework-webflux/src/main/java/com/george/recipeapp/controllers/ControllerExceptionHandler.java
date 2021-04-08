de7d1455a70fca58fa2eea94a35d1f610ee897bd
George Harpa <gheorghita.harpa0423@yahoo.com>
2021-04-08 22:51:40 +0100

    WebFlux Controller Advice.

--- aSpringFramework5-BeginnerToGuru/q-introduction-to-spring-framework-webflux/src/main/java/com/george/recipeapp/controllers/ControllerExceptionHandler.java
+++ aSpringFramework5-BeginnerToGuru/q-introduction-to-spring-framework-webflux/src/main/java/com/george/recipeapp/controllers/ControllerExceptionHandler.java
@@ -1,22 +1,25 @@
 package com.george.recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    /*@ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleBadRequest(Exception exception) {
-        log.error("Handling Number Format Exception");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleBadRequest(Exception exception, Model model) {
+        log.error("Handling Binding Exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
+        model.addAttribute("exception", exception);
 
-        return modelAndView;
-    }*/
+        return "400error";
+    }
 }
