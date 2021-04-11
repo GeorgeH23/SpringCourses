01ff4741bb64fcbe391842c485aeadb74902a4c2
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-04-11 21:03:31 +0100

    Introduction to Spring WebTestClient.

---
+++ aSpringFramework5-BeginnerToGuru/q-introduction-to-spring-framework-webflux/src/test/java/com/george/recipeapp/controllers/RouterFunctionTest.java
@@ -0,0 +1,57 @@
+package com.george.recipeapp.controllers;
+
+import com.george.recipeapp.config.WebConfig;
+import com.george.recipeapp.domain.Recipe;
+import com.george.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.Mockito.when;
+
+class RouterFunctionTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    RecipeService recipeService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        WebConfig webConfig = new WebConfig();
+
+        RouterFunction<?> routerFunction = webConfig.routes(recipeService);
+
+        webTestClient = WebTestClient.bindToRouterFunction(routerFunction).build();
+    }
+
+    @Test
+    void testGetRecipes() {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just());
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void testGetRecipesWithData() {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just(new Recipe(), new Recipe()));
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Recipe.class);
+    }
+}
