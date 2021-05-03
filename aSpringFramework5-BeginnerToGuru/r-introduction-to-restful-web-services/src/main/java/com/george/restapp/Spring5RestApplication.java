437984c7de5e1c230d353759c97dd25c15ca80cc
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-05-03 22:51:04 +0100

    Added new module to the project.

---
+++ aSpringFramework5-BeginnerToGuru/r-introduction-to-restful-web-services/src/main/java/com/george/restapp/Spring5RestApplication.java
@@ -0,0 +1,12 @@
+package com.george.restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RestApplication.class, args);
+    }
+}
---
+++ aSpringFramework5-BeginnerToGuru/r-introduction-to-restful-web-services/src/test/java/com/george/restapp/Spring5RestApplicationTest.java
@@ -0,0 +1,13 @@
+package com.george.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RestApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
