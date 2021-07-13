0ee4cfffe88dabdd696386ba831275ffad82809e
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-07-13 21:43:20 +0100

    Category Service.

---
+++ aSpringFramework5-BeginnerToGuru/s-restful-web-services-with-spring-mvc/src/test/java/com/george/springframework/services/CategoryServiceImplTest.java
@@ -0,0 +1,70 @@
+package com.george.springframework.services;
+
+import com.george.springframework.api.v1.mapper.CategoryMapper;
+import com.george.springframework.api.v1.model.CategoryDTO;
+import com.george.springframework.domain.Category;
+import com.george.springframework.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class CategoryServiceImplTest {
+
+    public static final Long ID = 2L;
+    public static final String NAME = "Jimmy";
+    CategoryService categoryService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+    }
+
+    @Test
+    void getAllCategories() throws Exception {
+
+        //given
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        //when
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+
+        //then
+        assertEquals(3, categoryDTOS.size());
+
+    }
+
+    @Test
+    void getCategoryByName() throws Exception {
+
+        //given
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+        //when
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+        //then
+        assertEquals(ID, categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+
+    }
+}
