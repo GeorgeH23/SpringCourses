f7b36a75f986382d0ed8e4557fcb9a6d602436bf
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-07-15 23:58:16 +0100

    List Categories

--- aSpringFramework5-BeginnerToGuru/s-restful-web-services-with-spring-mvc/src/main/java/com/george/springframework/Spring5MvcRestApplication.java
+++ aSpringFramework5-BeginnerToGuru/s-restful-web-services-with-spring-mvc/src/main/java/com/george/springframework/Spring5MvcRestApplication.java
@@ -2,8 +2,10 @@ package com.george.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.george.springframework.api.v1", "com.george.springframework"})
 public class Spring5MvcRestApplication {
 
     public static void main(String[] args) {
