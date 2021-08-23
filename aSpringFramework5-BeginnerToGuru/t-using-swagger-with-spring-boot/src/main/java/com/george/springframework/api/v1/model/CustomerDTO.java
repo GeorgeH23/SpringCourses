6e4a1c9b03698ada44e7d1ee2ec8acb5e73f6367
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-08-23 23:03:48 +0100

    Customizing Endpoint Documentation.

--- aSpringFramework5-BeginnerToGuru/t-using-swagger-with-spring-boot/src/main/java/com/george/springframework/api/v1/model/CustomerDTO.java
+++ aSpringFramework5-BeginnerToGuru/t-using-swagger-with-spring-boot/src/main/java/com/george/springframework/api/v1/model/CustomerDTO.java
@@ -1,6 +1,7 @@
 package com.george.springframework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,10 @@ import lombok.NoArgsConstructor;
 public class CustomerDTO {
 
     private Long id;
+    @ApiModelProperty(value = "This is the first name.", required = true)
     private String firstName;
+
+    @ApiModelProperty(value = "This is the last name.", required = true)
     private String lastName;
 
     @JsonProperty("customer_url")
