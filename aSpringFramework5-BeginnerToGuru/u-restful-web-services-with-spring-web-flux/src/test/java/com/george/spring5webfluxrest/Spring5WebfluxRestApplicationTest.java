306a3691d8ff963ab4ccb201629d6335bdeb67b9
George Harpa <gheorghita.harpa0423@yahoo.com>
2021-08-26 15:13:04 +0100

    Added u-restful-web-services-with-spring-web-flux module to the project.

---
+++ aSpringFramework5-BeginnerToGuru/u-restful-web-services-with-spring-web-flux/src/test/java/com/george/spring5webfluxrest/Spring5WebfluxRestApplicationTest.java
@@ -0,0 +1,13 @@
+package com.george.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
