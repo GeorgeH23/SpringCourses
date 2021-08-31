c11f3026b6451af61a7880cfaba52f4405213e2e
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-08-31 20:49:38 +0100

    Test GET endpoints for Vendors.

---
+++ aSpringFramework5-BeginnerToGuru/u-restful-web-services-with-spring-web-flux/src/test/java/com/george/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,62 @@
+package com.george.spring5webfluxrest.controllers;
+
+import com.george.spring5webfluxrest.domain.Vendor;
+import com.george.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController controller;
+
+    @BeforeEach
+    public void setUp() {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        controller = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(controller).build();
+    }
+
+    @Test
+    void list() {
+
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Fred").lastName("Flintstone").build(),
+                        Vendor.builder().firstName("Barney").lastName("Rubble").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(vendorRepository.findById("someid"))
+                .willReturn(Mono.just(Vendor.builder().firstName("Jimmy").lastName("Johns").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/someid")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+
+    @Test
+    void getByLastName() {
+        BDDMockito.given(vendorRepository.findByLastName("Weston"))
+                .willReturn(Mono.just(Vendor.builder().firstName("Michael").lastName("Weston").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/name/Weston")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+
+}
