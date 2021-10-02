736cfc5d71eed29cb3b3abcb54ab47e8a08c5b1b
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2021-10-02 21:49:25 +0100

    Added module w-spring-mvc-rest-docs to project.

---
+++ aSpringFramework5-BeginnerToGuru/w-spring-mvc-rest-docs/src/main/java/com/george/wspringmvcrestdocs/SpringMvcRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.george.wspringmvcrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcRestDocsApplication.class, args);
+    }
+
+}
