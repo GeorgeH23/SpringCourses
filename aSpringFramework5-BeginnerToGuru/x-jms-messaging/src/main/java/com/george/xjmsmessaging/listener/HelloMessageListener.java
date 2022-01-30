4c32b7fcfbe2efd9173a62511bb6c109000cda3b
Gheorghita Harpa <44534016+GeorgeH23@users.noreply.github.com>
2022-01-30 22:31:51 +0000

    Send and Receive of JMS Messages.

--- aSpringFramework5-BeginnerToGuru/x-jms-messaging/src/main/java/com/george/xjmsmessaging/listener/HelloMessageListener.java
+++ aSpringFramework5-BeginnerToGuru/x-jms-messaging/src/main/java/com/george/xjmsmessaging/listener/HelloMessageListener.java
@@ -2,20 +2,27 @@ package com.george.xjmsmessaging.listener;
 
 import com.george.xjmsmessaging.config.JmsConfig;
 import com.george.xjmsmessaging.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
@@ -25,5 +32,22 @@ public class HelloMessageListener {
 
         log.debug(String.valueOf(helloWorldMessage));
 
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        log.debug(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + " <-> I've got a message!");
+
+        log.debug(String.valueOf(helloMessage));
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
--- aSpringFramework5-BeginnerToGuru/x-jms-messaging/src/main/java/com/george/xjmsmessaging/sender/HelloSender.java
+++ aSpringFramework5-BeginnerToGuru/x-jms-messaging/src/main/java/com/george/xjmsmessaging/sender/HelloSender.java
@@ -1,5 +1,7 @@
 package com.george.xjmsmessaging.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.george.xjmsmessaging.config.JmsConfig;
 import com.george.xjmsmessaging.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
@@ -18,6 +22,7 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
@@ -33,5 +38,34 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
         log.debug("Message Sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+            Message helloMessage = null;
+
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "com.george.xjmsmessaging.model.HelloWorldMessage");
+
+                log.debug(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + " <-> I'm sending a \"Hello\"");
+
+                return helloMessage;
+            } catch (JsonProcessingException e) {
+                throw new JMSException("Ka-boom");
+            }
+        });
+
+        if (receivedMsg != null) {
+            log.debug(String.valueOf(receivedMsg.getBody(String.class)));
+        }
     }
 }
